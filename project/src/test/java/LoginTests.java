43a17f467965db38f11a57f5fe8a86e479447713
Darina Chernysheva <darina.chernysheva@techery.io>
2017-06-28 17:34:22 +0300

    Cleanup for LoginTests

--- project/src/test/java/LoginTests.java
+++ project/src/test/java/LoginTests.java
@@ -1,23 +1,15 @@
 import base.BaseTestWithDriver;
-import driver.DriverProvider;
-import org.apache.logging.log4j.Logger;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 import steps.DriverSteps;
 import steps.LoginSteps;
 import steps.NavigationSteps;
-import utils.DelayMeter;
 import utils.log.LogProvider;
-
-import java.util.concurrent.TimeUnit;
 
 public final class LoginTests extends BaseTestWithDriver implements LogProvider {
 
 	private LoginSteps loginSteps = getUiStepsComponent().loginSteps();
 	private NavigationSteps navigationSteps = getUiStepsComponent().navigationSteps();
 	private DriverSteps driverSteps = getUiStepsComponent().driverSteps();
-
-	private final Logger log = getLogger();
 
 	@Test
 	public void loginToAppAsFirstTimeUser() {
@@ -30,11 +22,6 @@ public final class LoginTests extends BaseTestWithDriver implements LogProvider 
 	public void loginToApp() {
 		loginSteps.loginIfRequired(defaultUser);
 		navigationSteps.assertLandingPageLoaded();
-	}
-
-	@AfterClass(alwaysRun = true)
-	public void reportDuration() {
-		DelayMeter.reportOperationDuration(DriverProvider.INIT_PAGES_OPERATION, TimeUnit.MILLISECONDS);
 	}
 }
 
